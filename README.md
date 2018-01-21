eee2283252ad67cc7f9f7f0f43dfdabfeca8ca6e
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2018-01-22 05:10:56 +0900

    Show the current status at the bottom
    - Refactor to use a class
    - Easy-to-view outputs
    - Improved README

--- README.md
+++ README.md
@@ -1,12 +1,51 @@
-# GNSS Status Viewer
+# gnss_status_viewer
 
-Just like how the [NTRIP Client](http://lefebure.com/software/ntripclient/)
-shows its logs.
+GNSS quality viewer. Shows the current fix quality (e.g. RTK, FloatRTK, DGPS),
+number of satellites observed, and the horizontal delusion of precision.
+
+
+## Example
+
+Here's what it looks like when running this program:
+
+```
+[INFO] [1516565360.176049, 1516434271.302132]: Number of satellites increased from 13 to 14
+[INFO] [1516565360.176934, 1516434271.302132]: HDOP decreased from 0.82 to 0.8
+
+[INFO] [1516565362.278148, 1516434273.404360]: Number of satellites decreased from 14 to 13
+[INFO] [1516565362.278737, 1516434273.404360]: HDOP increased from 0.8 to 0.89
+
+[INFO] [1516565362.480976, 1516434273.606435]: Fix quality changed from GPS to FloatRTK
+[INFO] [1516565362.481327, 1516434273.606435]: Number of satellites decreased from 13 to 7
+[INFO] [1516565362.481569, 1516434273.606435]: HDOP increased from 0.89 to 2.13
+
+[INFO] [1516565366.578242, 1516434277.697674]: Fix quality changed from FloatRTK to RTK
+[INFO] [1516565366.579443, 1516434277.697674]: Number of satellites decreased from 7 to 6
+[INFO] [1516565366.580347, 1516434277.697674]: HDOP increased from 2.13 to 3.94
+
+[INFO] [1516565366.778482, 1516434277.899627]: Fix quality changed from RTK to FloatRTK
+[INFO] [1516565366.779559, 1516434277.899627]: Number of satellites increased from 6 to 7
+[INFO] [1516565366.780447, 1516434277.899627]: HDOP decreased from 3.94 to 1.91
+
+[INFO] [1516565366.877559, 1516434278.000723]: Fix quality changed from FloatRTK to RTK
+[INFO] [1516565366.878753, 1516434278.000723]: HDOP increased from 1.91 to 2.6
+
+[INFO] [1516565366.980365, 1516434278.101749]: HDOP decreased from 2.6 to 1.82
+
+[INFO] [1516565367.079611, 1516434278.202733]: HDOP decreased from 1.82 to 1.8
+
+Quality :       RTK
+Num Sat :         7
+HDOP    :       1.8
+
+```
+
 
 ## License
 
 MIT
 
+
 ## Author
 
-Naoki Mizuno
+Naoki Mizuno (mizuno.naoki@rm.is.tohoku.ac.jp)
---
+++ src/gnss_status_viewer/status.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python2
+
+
+class Status:
+    FIX_QUALITY = {
+        None: 'No Data',
+        '': 'Empty GGA',
+        '0': 'Invalid',
+        '1': 'GPS',
+        '2': 'DGPS',
+        '4': 'RTK',
+        '5': 'FloatRTK',
+    }
+
+    def __init__(self, gga):
+        self.is_gga = False
+        self.fix = None
+        self.num_sat = None
+        self.hdop = None
+
+        tokens = gga.strip().split(',')
+        if tokens[0] != '$GPGGA':
+            return
+
+        self.fix = tokens[6]
+        self.num_sat = int(tokens[7])
+        self.hdop = float(tokens[8])
+        self.is_gga = True
+
+    def __str__(self):
+        q = 'Quality : {0:>9}'.format(Status.FIX_QUALITY[self.fix])
+        s = 'Num Sat : {0:>9}'.format(self.num_sat)
+        h = 'HDOP    : {0:>9}'.format(self.hdop)
+        return '\n'.join((q, s, h))
+
+    def __eq__(self, other):
+        if not isinstance(other, Status):
+            return False
+        return self.fix == other.fix and \
+            self.num_sat == other.num_sat and \
+            self.hdop == other.hdop
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    @staticmethod
+    def get_status_change(a, b):
+        """
+        Returns a list of the string that represents the change from a to b
+
+        :param a: status before
+        :param b: status after
+        :return the strings representing the changes between a and b
+        """
+        if not isinstance(a, Status) or not isinstance(b, Status):
+            return ''
+
+        s = []
+        if a.fix != b.fix:
+            s.append('Fix quality changed from {0} to {1}'.format(
+                Status.FIX_QUALITY[a.fix],
+                Status.FIX_QUALITY[b.fix]
+            ))
+        if a.num_sat != b.num_sat:
+            s.append('Number of satellites {0} from {1} to {2}'.format(
+                Status.__get_change_str__(a.num_sat, b.num_sat),
+                a.num_sat,
+                b.num_sat
+            ))
+        if a.hdop != b.hdop:
+            s.append('HDOP {0} from {1} to {2}'.format(
+                Status.__get_change_str__(a.hdop, b.hdop),
+                a.hdop,
+                b.hdop
+            ))
+        return s
+
+    @staticmethod
+    def __get_change_str__(a, b):
+        return 'increased' if a < b else 'decreased'
